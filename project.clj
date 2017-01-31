fda4825e6f463f3aca7f4db13cc601da46b3b867
Daniel Carleton <dacc@uw.edu>
2017-01-31 15:57:00 -0800

    depend on stable clojure, add url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject uwcpdx/bean-dip "0.3.0"
   :description "Bidirectional translation between maps and Java beans that's declarative and reflection-free."
-  :url ""
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]]
+  :url "https://github.com/uwcpdx/bean-dip"
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :java-source-paths ["java"])
 
