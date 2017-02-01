9f374db3f42b1063e04e56dc2027840f79871261
Daniel Carleton <dacc@uw.edu>
2017-01-31 16:19:50 -0800

    switch to simple group name

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
-(defproject uwcpdx/bean-dip "0.3.0"
+(defproject uwcpdx/bean-dip "0.4.0"
   :description "Bidirectional translation between maps and Java beans that's declarative and reflection-free."
   :url "https://github.com/uwcpdx/bean-dip"
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :java-source-paths ["java"])
+  :java-source-paths ["java"]
+  :deploy-repositories {"clojars" {:url           "https://clojars.org/repo"
+                                   :username      :env/CLOJARS_USERNAME
+                                   :password      :env/CLOJARS_PASSWORD
+                                   :sign-releases false}})
 
