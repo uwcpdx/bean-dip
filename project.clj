8ed8b2051e1e9871c9915cf4c3c0a201c338f607
Daniel Carleton <dacc@uw.edu>
2017-06-09 12:15:15 -0700

    pin 0.6.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject uwcpdx/bean-dip "0.6.0-SNAPSHOT"
+(defproject uwcpdx/bean-dip "0.6.0"
   :description "Bidirectional translation between maps and Java beans that's declarative and reflection-free."
   :url "https://github.com/uwcpdx/bean-dip"
   :dependencies [[org.clojure/clojure "1.8.0"]]
