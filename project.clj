5832e41f16619f7b5a2bac13b5fe8e18046bb9aa
Daniel Carleton <dacc@uw.edu>
2017-06-19 12:35:18 -0700

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject uwcpdx/bean-dip "0.7.1"
+(defproject uwcpdx/bean-dip "0.7.2-SNAPSHOT"
   :description "Bidirectional translation between maps and Java beans that's declarative and reflection-free"
   :url "https://github.com/uwcpdx/bean-dip"
   :dependencies [[org.clojure/clojure "1.8.0"]]
